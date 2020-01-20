6dfe05dddf62d0809a2444e3ee349e0e48c07f3c
Kuleshov Dmitry <dkuleshov@lohika.com>
2020-01-20 22:53:45 +0200

    fix

--- src/com/lohika/GlobalConfig.groovy
+++ src/com/lohika/GlobalConfig.groovy
@@ -2,10 +2,6 @@ package com.lohika
 
 class GlobalConfig {
     public static defaults = [
-            "components": [
-                    "backend",
-                    "frontend"
-            ],
             "k8s" : [
                     "dev": [
                             "clusterUrl": "k8s.local",
