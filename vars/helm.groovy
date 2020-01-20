c0c3b0ca50c269fce4be36285fd9d51f07db26f7
Kuleshov Dmitry <dkuleshov@lohika.com>
2020-01-20 22:40:52 +0200

    add cfg param

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,14 +1,19 @@
-def repoUpdate() {
+import static com.lohika.GlobalConfig.defaults
+
+ef repoUpdate() {
     sh 'helm repo update'
 }
 
-def upgrade(String name, String chart = '', String args = '', def cfg = [:]) {
+def upgrade(String name, String chart = '', String args = '', String cluster = '') {
     def chartName = chart ? chart : "${name}/"
     echo chart
     echo chartName
-    if (cfg) {
-        echo "Config is not empty"
-        withKubeConfig([credentialsId: cfg.credentialsId, serverUrl: cfg.clusterUrl]) {
+    if (cluster) {
+        def credentialsId = defaults.k8s[cluster.toLowerCase()].credentialsId
+        def clusterUrl = defaults.k8s[cluster.toLowerCase()].clusterUrl
+        println(credentialsId)
+        println(clusterUrl)
+        withKubeConfig([credentialsId: credentialsId, serverUrl: clusterUrl]) {
             sh "helm upgrade -i ${name} ${chartName} ${args}"
         }
     } else {
